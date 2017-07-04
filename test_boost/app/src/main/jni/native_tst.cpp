76d8cb097632c2ceae20d60061d18e5710ecd188
Declan <dec1@geoaze.com>
2017-07-04 13:08:30 +0200

    actually use boost (chrono) functions

--- test_boost/app/src/main/jni/native_tst.cpp
+++ test_boost/app/src/main/jni/native_tst.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <jni.h>
 
+#include <boost/chrono/chrono.hpp>
+#include <boost/lexical_cast.hpp>
 using std::string;
 
 //----------------------------------------------------------------------
@@ -17,29 +19,28 @@ extern "C"
         string Str("hi from C++, boost should give some meaningful info here \:");
 
 
-        // #include <boost/chrono/
-        // #include <boost/chrono/chrono.hpp>
 
 
-//        boost::chrono::system_clock::time_point p  = boost::chrono::system_clock::now();
-//        std::time_t t = boost::chrono::system_clock::to_time_t(p);
-//
-//        char buffer[26];
-//        ctime_r(&t, buffer);
-//
-//        //  std::string tst = std::to_string(3);
-//
-//        int ver = BOOST_VERSION;
-//        int ver_maj = ver/100000;
-//        int ver_min = ver / 100 %1000;
-//        int ver_pat = ver %100;
-//
-//        string Ver_Maj = boost::lexical_cast<string>(ver_maj);
-//        string Ver_Min = boost::lexical_cast<string>(ver_min);
-//        string Ver_Pat = boost::lexical_cast<string>(ver_pat);
-//
-//        Str += "\n Boost version: " + Ver_Maj + "." + Ver_Min + "." + Ver_Pat + "\n";
-//        Str += "... says time is " + std::string(buffer) + "\n\n";
+
+        boost::chrono::system_clock::time_point p  = boost::chrono::system_clock::now();
+        std::time_t t = boost::chrono::system_clock::to_time_t(p);
+
+        char buffer[26];
+        ctime_r(&t, buffer);
+
+        //  std::string tst = std::to_string(3);
+
+        int ver = BOOST_VERSION;
+        int ver_maj = ver/100000;
+        int ver_min = ver / 100 %1000;
+        int ver_pat = ver %100;
+
+        string Ver_Maj = boost::lexical_cast<string>(ver_maj);
+        string Ver_Min = boost::lexical_cast<string>(ver_min);
+        string Ver_Pat = boost::lexical_cast<string>(ver_pat);
+
+        Str += "\n Boost version: " + Ver_Maj + "." + Ver_Min + "." + Ver_Pat + "\n";
+        Str += "... says time is " + std::string(buffer) + "\n\n";
 
 
         return Env->NewStringUTF(Str.c_str());
