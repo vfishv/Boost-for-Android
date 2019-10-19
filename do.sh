96cdb89447aa8163816944a1d36155d935d53a95
Declan <dec1@geoaze.com>
2019-10-19 18:58:01 +0200

    hide as and strip as they're not being called

--- do.sh
+++ do.sh
@@ -4,10 +4,10 @@ export BOOST_DIR=$(pwd)/../down/boost/1.71.0
 
 export NDK_DIR=$(pwd)/../down/ndk/20
 
-export ABI_NAMES="armeabi-v7a" 
-#export ABI_NAMES="arm64-v8a armeabi-v7a x86 x86_64"
+#export ABI_NAMES="armeabi-v7a" 
+export ABI_NAMES="arm64-v8a armeabi-v7a x86 x86_64"
 
-export LINKAGES="static"
-#export LINKAGES="shared static" # can be "shared" or "static" or "shared static" (both)
+#export LINKAGES="static"
+export LINKAGES="shared static" # can be "shared" or "static" or "shared static" (both)
 
 ./build.sh
